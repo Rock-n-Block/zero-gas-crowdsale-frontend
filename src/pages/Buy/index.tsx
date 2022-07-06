520d00bf67bf0334617f37d385a34a809b654359
Nezort11 <nezort11@gmail.com>
2022-07-06 18:14:34 +0300

    Replate with coming soon

--- src/pages/Buy/index.tsx
+++ src/pages/Buy/index.tsx
@@ -52,96 +52,9 @@ const Buy = () => {
           </Button>
         </div>
         <div className={s.content}>
-          <div>
-            <Typography className={s.title} type="h1" fontFamily="DrukCyr Wide" weight={900}>
-              Buy
-            </Typography>
-            <Typography className={s.title} type="h1" fontFamily="DrukCyr Wide" weight={900}>
-              Zer <ZeroGasIcon className={s.zGasIcon} /> gas
-            </Typography>
-          </div>
-          <div className={s.cards}>
-            <div className={s.card}>
-              <Progress value={Math.floor(totalBought / hardcap)} className={s.progress}>
-                <Typography type="body2">
-                  Sold{' '}
-                  <Typography type="body2" weight={600} className={s.displayInline}>
-                    {totalBought}{' '}
-                  </Typography>
-                  out of{' '}
-                  <Typography type="body2" weight={600} className={s.displayInline}>
-                    {getFormatNumber(hardcap)}
-                  </Typography>
-                </Typography>
-              </Progress>
-            </div>
-            <div className={s.card}>
-              <Typography
-                type="body1"
-                fontFamily="DrukCyr Wide"
-                weight={900}
-                className={s.stageWillStart}
-              >
-                {currentStage && stageTexts[currentStage]}
-              </Typography>
-              <Timer
-                days={timeLeft.days}
-                hours={timeLeft.hours}
-                minutes={timeLeft.minutes}
-                seconds={timeLeft.seconds}
-              />
-            </div>
-            <div className={s.card}>
-              <Typography
-                type="body1"
-                fontFamily="DrukCyr Wide"
-                weight={900}
-                className={s.currentListTitle}
-              >
-                Current prices
-              </Typography>
-              {Object.values(tokens).map((token) => (
-                <div key={token.address} className={s.currencyListItem}>
-                  <img
-                    src={token.image}
-                    alt={`${token.symbol} logo`}
-                    className={s.currencyListItemImage}
-                    width={48}
-                    height={48}
-                  />
-                  <Typography type="body1">
-                    {token.fullName} ({token.symbol})
-                  </Typography>
-                  <Typography type="body1" className={s.currencyListItemPrice}>
-                    ${getFormatFiat(token?.value)}
-                  </Typography>
-                </div>
-              ))}
-              <div className={s.currencyListItem}>
-                <ZeroGasIcon className={s.currencyListItemImage} width={48} height={48} />
-                <Typography type="body1">Zerogas (0GAS)</Typography>
-                <Typography type="body1" className={s.currencyListItemPrice}>
-                  ${getFormatFiat(zeroGasPrice)}
-                </Typography>
-              </div>
-            </div>
-          </div>
-          <div className={s.card}>
-            <Typography
-              type="body1"
-              fontFamily="DrukCyr Wide"
-              weight={900}
-              className={s.stageWillStart}
-            >
-              {currentStage && stageTexts[currentStage]}
-            </Typography>
-            <Timer
-              days={timeLeft.days}
-              hours={timeLeft.hours}
-              minutes={timeLeft.minutes}
-              seconds={timeLeft.seconds}
-            />
-          </div>
+          <Typography type="h2" className={s.comingSoon}>
+            Coming soon
+          </Typography>
         </div>
       </div>
     </div>
