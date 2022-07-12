b7719b4ea109854c8a8bdc566455b132a8dc9980
Nezort11 <nezort11@gmail.com>
2022-07-12 13:33:20 +0300

    Add mobile parallax

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,7 @@ import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 import { WalletModal } from '@/components';
 import { Footer } from '@/containers';
-import { useShallowSelector } from '@/hooks';
+import { useBreakpoints, useShallowSelector } from '@/hooks';
 import { closeModal } from '@/store/modals/reducer';
 import modalsSelectors from '@/store/modals/selectors';
 import { Modals } from '@/types';
@@ -14,6 +14,7 @@ import { AboutUs, Main, Partners, RoadMap, Tokenomics } from './containers';
 const Home = () => {
   const { activeModal } = useShallowSelector(modalsSelectors.getProp('modalState'));
   const dispatch = useDispatch();
+  const [isMobile] = useBreakpoints([541]);
 
   const handleActiveModalClose = useCallback(() => {
     dispatch(closeModal());
@@ -21,25 +22,46 @@ const Home = () => {
 
   return (
     <>
-      <Parallax pages={7.73} enabled>
-        <ParallaxLayer offset={0} speed={0}>
-          <Main />
-        </ParallaxLayer>
-        <ParallaxLayer offset={1.2} speed={0.5}>
-          <AboutUs />
-        </ParallaxLayer>
-        <ParallaxLayer offset={2.9} speed={1}>
-          <Tokenomics />
-        </ParallaxLayer>
-        <ParallaxLayer offset={3.9} speed={0.4}>
-          <RoadMap />
-        </ParallaxLayer>
-        <ParallaxLayer offset={6.3} speed={1.3} factor={0.5}>
-          <Partners />
-          <Footer />
-        </ParallaxLayer>
-      </Parallax>
-
+      {isMobile === true && (
+        <Parallax pages={14.13} enabled>
+          <ParallaxLayer offset={0} speed={0}>
+            <Main />
+          </ParallaxLayer>
+          <ParallaxLayer offset={1.0} speed={0.7}>
+            <AboutUs />
+          </ParallaxLayer>
+          <ParallaxLayer offset={3.9} speed={0.5}>
+            <Tokenomics />
+          </ParallaxLayer>
+          <ParallaxLayer offset={5.99999} speed={0.4}>
+            <RoadMap />
+          </ParallaxLayer>
+          <ParallaxLayer offset={10.4} speed={0.5}>
+            <Partners />
+            <Footer />
+          </ParallaxLayer>
+        </Parallax>
+      )}
+      {isMobile === false && (
+        <Parallax pages={7.74} enabled>
+          <ParallaxLayer offset={0} speed={0}>
+            <Main />
+          </ParallaxLayer>
+          <ParallaxLayer offset={1.2} speed={0.5}>
+            <AboutUs />
+          </ParallaxLayer>
+          <ParallaxLayer offset={2.9} speed={1}>
+            <Tokenomics />
+          </ParallaxLayer>
+          <ParallaxLayer offset={3.9} speed={0.4}>
+            <RoadMap />
+          </ParallaxLayer>
+          <ParallaxLayer offset={6.3} speed={1.3}>
+            <Partners />
+            <Footer />
+          </ParallaxLayer>
+        </Parallax>
+      )}
       <WalletModal visible={activeModal === Modals.Wallet} onClose={handleActiveModalClose} />
     </>
   );
