f0cbaed966a38c8fca35cfb23e057283bb466893
Nezort11 <nezort11@gmail.com>
2022-07-12 11:45:18 +0300

    Add desktop parallax

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 import { WalletModal } from '@/components';
+import { Footer } from '@/containers';
 import { useShallowSelector } from '@/hooks';
 import { closeModal } from '@/store/modals/reducer';
 import modalsSelectors from '@/store/modals/selectors';
@@ -20,11 +21,24 @@ const Home = () => {
 
   return (
     <>
-      <Main />
-      <AboutUs />
-      <Tokenomics />
-      <RoadMap />
-      <Partners />
+      <Parallax pages={7.73} enabled>
+        <ParallaxLayer offset={0} speed={0}>
+          <Main />
+        </ParallaxLayer>
+        <ParallaxLayer offset={1.2} speed={0.5}>
+          <AboutUs />
+        </ParallaxLayer>
+        <ParallaxLayer offset={2.9} speed={1}>
+          <Tokenomics />
+        </ParallaxLayer>
+        <ParallaxLayer offset={3.9} speed={0.4}>
+          <RoadMap />
+        </ParallaxLayer>
+        <ParallaxLayer offset={6.3} speed={1.3} factor={0.5}>
+          <Partners />
+          <Footer />
+        </ParallaxLayer>
+      </Parallax>
 
       <WalletModal visible={activeModal === Modals.Wallet} onClose={handleActiveModalClose} />
     </>
