9815afe244f6099f597160cdbb714137b5650653
Egor Zorin <nezort11@gmail.com>
2022-07-12 18:15:58 +0300

    Add react scroll parallax (#16)

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,62 +1,19 @@
 import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import { Parallax } from 'react-scroll-parallax';
 
 import { WalletModal } from '@/components';
 import { Footer } from '@/containers';
-import { useBreakpoints, useShallowSelector } from '@/hooks';
+import { useShallowSelector } from '@/hooks';
 import { closeModal } from '@/store/modals/reducer';
 import modalsSelectors from '@/store/modals/selectors';
 import { Modals } from '@/types';
 
 import { AboutUs, Main, Partners, RoadMap, Tokenomics } from './containers';
 
-const DesktopLayout = () => (
-  <Parallax pages={7.74} enabled>
-    <ParallaxLayer offset={0} speed={0}>
-      <Main />
-    </ParallaxLayer>
-    <ParallaxLayer offset={1.2} speed={0.5}>
-      <AboutUs />
-    </ParallaxLayer>
-    <ParallaxLayer offset={2.9} speed={1}>
-      <Tokenomics />
-    </ParallaxLayer>
-    <ParallaxLayer offset={3.9} speed={0.4}>
-      <RoadMap />
-    </ParallaxLayer>
-    <ParallaxLayer offset={6.3} speed={1.3}>
-      <Partners />
-      <Footer />
-    </ParallaxLayer>
-  </Parallax>
-);
-
-const MobileLayout = () => (
-  <Parallax pages={14.13} enabled>
-    <ParallaxLayer offset={0} speed={0}>
-      <Main />
-    </ParallaxLayer>
-    <ParallaxLayer offset={1.0} speed={0.7}>
-      <AboutUs />
-    </ParallaxLayer>
-    <ParallaxLayer offset={3.9} speed={0.5}>
-      <Tokenomics />
-    </ParallaxLayer>
-    <ParallaxLayer offset={5.99999} speed={0.4}>
-      <RoadMap />
-    </ParallaxLayer>
-    <ParallaxLayer offset={10.4} speed={0.5}>
-      <Partners />
-      <Footer />
-    </ParallaxLayer>
-  </Parallax>
-);
-
 const Home = () => {
   const { activeModal } = useShallowSelector(modalsSelectors.getProp('modalState'));
   const dispatch = useDispatch();
-  const [isMobile] = useBreakpoints([541]);
 
   const handleActiveModalClose = useCallback(() => {
     dispatch(closeModal());
@@ -64,7 +21,24 @@ const Home = () => {
 
   return (
     <>
-      {isMobile ? <MobileLayout /> : <DesktopLayout />}
+      <Parallax>
+        <Main />
+      </Parallax>
+      <Parallax speed={15}>
+        <AboutUs />
+      </Parallax>
+      <Parallax speed={-13}>
+        <Tokenomics />
+      </Parallax>
+      <Parallax speed={20}>
+        <RoadMap />
+      </Parallax>
+      <Parallax speed={-20}>
+        <Partners />
+      </Parallax>
+      <Parallax speed={0}>
+        <Footer />
+      </Parallax>
       <WalletModal visible={activeModal === Modals.Wallet} onClose={handleActiveModalClose} />
     </>
   );
