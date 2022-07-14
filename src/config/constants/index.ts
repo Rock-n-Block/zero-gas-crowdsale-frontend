498c0ae66864c36a56ef10588cc0f49c1430fe87
Nezort11 <nezort11@gmail.com>
2022-07-14 20:59:04 +0300

    Fix issues

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -2,3 +2,5 @@ export const isMainnet = !!process.env.REACT_IS_MAINNET;
 
 export const ETHER_DECIMALS = 18;
 export const ETHER_ADDRESS = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
+
+export const ZEROGAS_DECIMALS = 18;
